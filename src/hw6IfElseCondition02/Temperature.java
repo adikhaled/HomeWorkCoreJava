59f9d418c4623f113607aba0919edeb609d02ee6
adikhaled <khldzmnd@gmail.com>
2022-08-12 15:28:11 -0400

    hw update

--- src/hw6IfElseCondition02/Temperature.java
+++ src/hw6IfElseCondition02/Temperature.java
@@ -8,26 +8,20 @@ public class Temperature {
 	public static void main(String[] args) {
 		System.out.println("today temperature:");
 		Scanner scanner = new Scanner(System.in);
-		int toadaysTemperature = scanner.nextInt();
+		int todaysTemperature = scanner.nextInt();
 
-		
-		if(toadaysTemperature<32) {
+		if (todaysTemperature < 32) {
 			System.out.println("Freezing");
-		}else if(toadaysTemperature<55) {
+		} else if (todaysTemperature < 55) {
 			System.out.println("Pleasant");
-		}else if (toadaysTemperature<73) {
+		} else if (todaysTemperature < 73) {
 			System.out.println("Getting Warmer");
-		}else if(toadaysTemperature<101) {
+		} else if (todaysTemperature < 101) {
 			System.out.println("Hot");
 		} else {
 			System.out.println("Ice on my Head Please");
 		}
-			
+
 	}
 
 }
-
-		
-	
-
-
