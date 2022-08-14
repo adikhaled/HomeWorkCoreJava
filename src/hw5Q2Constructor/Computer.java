be3253be740ad1853cdb621409c36e9cfae3a593
adikhaled <khldzmnd@gmail.com>
2022-08-13 21:29:40 -0400

    update

--- src/hw5Q2Constructor/Computer.java
+++ src/hw5Q2Constructor/Computer.java
@@ -1,4 +1,4 @@
-package hw5Q2Constructor; 
+package hw5Q2Constructor;
 
 public class Computer {
 	// variable declare
@@ -9,19 +9,22 @@ public class Computer {
 	public int price;
 	public char grade;
 	public boolean madeInUsa;
-	
+
 	// default constructor
-public Computer(){
-	System.out.println("This is from default Constructor of Computer class");
-		
-}	 //Parameterize constructor
-		public Computer(String brand){
+	public Computer() {
+		System.out.println("This is from default Constructor of Computer class");
+
+	} // Parameterize constructor
+
+	public Computer(String brand, int price, char grade, boolean madeInUsa) {
 		this.brand = brand;
 		System.out.println("This Computer brand: " + brand);
-			
+
 	}
-		
-public Computer(String brand,String model,String operatingSystem,int price,char grade,boolean madeInUsa){
-System.out.println("My Computer brand is : " + brand + ", My Computer Model is: " + model + ", My Computer operating system is: " + operatingSystem + ", My Computer price is: " + price + ", My Computer grade is: "  + grade + ", My Computer is Made in Usa: " + madeInUsa + ".");	
-}
+
+	public Computer(String brand, String model, String operatingSystem, int price, char grade, boolean madeInUsa) {
+		System.out.println("My Computer brand is : " + brand + ", My Computer Model is: " + model
+				+ ", My Computer operating system is: " + operatingSystem + ", My Computer price is: " + price
+				+ ", My Computer grade is: " + grade + ", My Computer is Made in Usa: " + madeInUsa + ".");
+	}
 }
